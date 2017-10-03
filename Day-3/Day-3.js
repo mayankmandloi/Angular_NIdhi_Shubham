304f61edde0befbc75851a49ae251d71c229ece4
mayankmandloi <mayankmandloi009@gmail.com>
2017-10-03 10:03:18 +0530

    .

---
+++ Day-3/Day-3.js
@@ -0,0 +1,9 @@
+angular.module("myModule",[]);
+
+angular.module("myModule").controller("nidhiController",function () {
+  this.person={name:"Nidhi",email:"nidhi.nidhi999@gmail.com"};
+});
+
+angular.module("myModule").controller("shubhamController",function () {
+  this.person={name:"Shubham",email:"snirmal763@gmail.com"};
+});
