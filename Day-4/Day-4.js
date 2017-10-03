0eadca3f180c52604ba9a9b9c710a7872e89201f
mayankmandloi <mayankmandloi009@gmail.com>
2017-10-03 10:18:14 +0530

    .

---
+++ Day-4/Day-4.js
@@ -0,0 +1,11 @@
+angular.module("myModule",[]);
+angular.module("myModule").controller("myController",function () {
+  this.persons=[{name:"Nidhi",number:1234567,email:"nidhi@webbhumi.com"},
+  {name:"Shubham",number:9876554,email:"shubham@webbhumi.com"},
+  {name:"Shivam",number:420420,email:"shivam@webbhumi.com"},
+  {name:"Neha",number:100100,email:"neha@webbhumi.com"},
+  {name:"Nidhi",number:1234567,email:"nidhi@webbhumi.com"},
+  {name:"Shubham",number:9876554,email:"shubham@webbhumi.com"},
+  {name:"Shivam",number:420420,email:"shivam@webbhumi.com"},
+  {name:"Neha",number:100100,email:"neha@webbhumi.com"}];
+});
