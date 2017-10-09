f9261d1a896c8ed8c5f762e1d1ace4b05968f82d
mayankmandloi <mayankmandloi009@gmail.com>
2017-10-09 10:35:02 +0530

    ..

---
+++ Day-7/Day-7.js
@@ -0,0 +1,4 @@
+angular.module("checkFilterModule",[])
+angular.module("checkFilterModule").controller("forCurrencyController",function () {
+  this.lateFee=[{name:"Nidhi",fee:10},{name:"Shubham",fee:10},{name:"Shivam",fee:0},{name:"Mayank",fee:0}];
+})
