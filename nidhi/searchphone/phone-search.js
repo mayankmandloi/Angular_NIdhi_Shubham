f7f2e180a3cf4a115bda75fffbb2d7e2b3dd9410
Nidhi <monavyas55@gmail.com>
2017-10-09 23:32:53 +0530

    search phone

---
+++ nidhi/searchphone/phone-search.js
@@ -0,0 +1,31 @@
+angular.module("myModule",[]);
+angular.module("myModule").controller("myController",function () {
+  this.phone=[
+  {
+    "name": "Xiamoi Redmi 4A 3GB RAM",
+    "totalReviewsCount": 1200,
+    "posReviewPerc": 70,
+    "negReviewPerc": 30,
+    "price": 5000,
+    "currency": "INR",
+    "imageUrl": "https://storage.googleapis.com/producttalk/image1.jpeg"
+  },
+  {
+    "name": "Samsung",
+    "totalReviewsCount": 1200,
+    "posReviewPerc": 70,
+    "negReviewPerc": 30,
+    "price": 5000,
+    "currency": "INR",
+    "imageUrl": "https://storage.googleapis.com/producttalk/image1.jpeg"
+  },
+  {
+    "name": "Sony",
+    "totalReviewsCount": 1200,
+    "posReviewPerc": 70,
+    "negReviewPerc": 30,
+    "price": 5000,
+    "currency": "INR",
+    "imageUrl": "https://storage.googleapis.com/producttalk/image1.jpeg"
+  }]
+});
