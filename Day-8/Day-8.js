4e2b34f4ca2b32cf61002c25ecd78f4bfd6cbabb
mayankmandloi <mayankmandloi009@gmail.com>
2017-10-11 10:22:38 +0530

    ...

---
+++ Day-8/Day-8.js
@@ -0,0 +1,41 @@
+angular.module("myModule",[]);
+angular.module("myModule").controller("myController",function ($scope) {
+  $scope.bheed=[{name:"nidhi",Rnum:001},{name:"shubham",Rnum:002},{name:"shivam",Rnum:007},{name:"suraj",Rnum:008}]
+});
+
+angular.module("myModule").filter("countchar",function () {
+  return function (item) {
+    return item.length;
+  }
+});
+
+
+
+angular.module("myModule").filter("firstcaps",function () {
+  return function (item) {
+    var first= item[0];
+    var rest=item.slice(1);
+    first=first.toUpperCase();
+    return first+rest;
+  };
+
+});
+  angular.module("myModule").filter("fine",function () {
+    return function (item) {
+      if (item=="nidhi") {
+        return 20;
+      }
+    else if (item=="shubham") {
+      return 20;
+    }
+  else if (item=="mayank") {
+    return 20;
+  }
+else if (item=="shivam") {
+  return 0;
+}
+else {
+  return 0
+}
+    };
+});
